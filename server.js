144fda7644d65ad7411bf395e33ccea7410b64f2
TiTa05 <stevensmex05@gmail.com>
2025-09-17 02:47:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ require('dotenv').config();
 const http = require('http');
 const { Server } = require('socket.io');
 const chatSocket = require('./sockets/chat.socket');
+const server = http.createServer(app);
 
 // Créer une instance d'Express
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware pour parser les requêtes JSON
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:4200'], // adapte selon ton frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Vérifier que JWT_SECRET est bien défini
@@ -44,10 +50,14 @@ app.use('/api/v1/chat', require('./routes/chatRoutes'));
 //app.use('/auth', require('./routes/forgotPasswordRoutes'));
 //app.use('/auth', require('./routes/resetPasswordRoutes'));
 
-const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: '*' }
+  cors: {
+    origin: ['http://localhost:4200'],
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
 });
+
 
 chatSocket(io);
 
