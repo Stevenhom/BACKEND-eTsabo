fc77ba236d8f87291ed950be6b9d6c957b7a60db
TiTa05 <stevensmex05@gmail.com>
2025-09-17 03:17:41 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: process.env.POSTGRES_HOST || 'localhost',
   port: process.env.POSTGRES_PORT || 5432,
-  timezone: '+03:00',
+  // timezone: '+03:00',
   dialect: 'postgres',
   logging: false, // Active si tu veux voir les requêtes SQL
 });
